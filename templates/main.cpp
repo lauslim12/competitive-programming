e2fe9894602788d4cdb6ed65ee4818a6f5f03cea
lauslim12 <nicholasdwiarto@yahoo.com>
2020-08-12 12:58:24 +0700

    Added 'solve' function for the C++ solution template

--- templates/main.cpp
+++ templates/main.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 using namespace std;
- 
+
 #define IOS                \
   ios::sync_with_stdio(0); \
   cin.tie(0);              \
   cout.tie(0);
 #define endl "\n";
- 
+
+int solve() {
+  // Solution here.
+  return 0;
+}
+
 int main() {
   // Solve the problem!
   IOS;
- 
+  solve();
+
   return 0;
 }
