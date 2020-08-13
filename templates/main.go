a7394a378d472342209098621d25c26a7c5ef737
lauslim12 <nicholasdwiarto@yahoo.com>
2020-08-13 12:04:00 +0700

    Fixed several comments in the Golang template.

--- templates/main.go
+++ templates/main.go
@@ -10,19 +10,19 @@ import (
 var reader *bufio.Reader = bufio.NewReader(os.Stdin)
 var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
 
-// Helper functions to wrap around the buffered IO.
+// Helper function to wrap around the buffered IO.
 func printf(f string, a ...interface{}) {
 	fmt.Fprintf(writer, f, a...)
 }
 
-// For this function, don't forget to add '\n' every scanf() call.
+// Helper function to take an input from the user.
 func scanf(f string, a ...interface{}) {
 	fmt.Fscanf(reader, f, a...)
 }
 
 // Solve
 func solve() {
-	// Solution
+	// Solve the problem!
 }
 
 func main() {
