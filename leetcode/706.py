ebc273001798c753b9e6b6ae93ef9134bb1b2122
lauslim12 <nicholasdwiarto@yahoo.com>
2021-02-01 17:21:23 +0700

    Leetcode #706 - Design HashMap

---
+++ leetcode/706.py
@@ -0,0 +1,43 @@
+class MyHashMap:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.hashmap = {}
+        
+
+    def put(self, key: int, value: int) -> None:
+        """
+        value will always be non-negative.
+        """
+        self.hashmap[key] = value
+        
+
+    def get(self, key: int) -> int:
+        """
+        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+        """
+        if self.hashmap.get(key) is None:
+            return -1
+        
+        return self.hashmap.get(key)
+        
+        
+
+    def remove(self, key: int) -> None:
+        """
+        Removes the mapping of the specified value key if this map contains a mapping for the key
+        """
+        if self.hashmap.get(key) is None:
+            return
+        
+        del self.hashmap[key]
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
