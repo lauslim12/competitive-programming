c1d1bdae19df22ec79c305d760026e9e03cef6b1
lauslim12 <nicholasdwiarto@yahoo.com>
2021-02-06 16:15:06 +0700

    Leetcode #412 - Fizz Buzz

---
+++ leetcode/412.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+  const ans = [];
+  
+  for (let i = 1; i <= n; i += 1) {
+      if (i % 3 === 0 && i % 5 === 0) {
+          ans.push("FizzBuzz");
+      } else if (i % 5 === 0) {
+          ans.push("Buzz");
+      } else if (i % 3 === 0) {
+          ans.push("Fizz");
+      } else {
+          ans.push(i.toString());
+      }   
+  }
+  
+  return ans;
+};
