2dcf2cfea423797d21f022203b9aad9e0b4d68a2
lauslim12 <nicholasdwiarto@yahoo.com>
2021-02-17 16:26:06 +0700

    Leetcode #142 - Linked List Cycle II

---
+++ leetcode/142.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        ans = set()
+        
+        while head:
+            if head not in ans:
+                ans.add(head)
+            else:
+                return head
+
+            head = head.next
+        
+        return
